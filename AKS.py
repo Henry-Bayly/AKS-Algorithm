2891ffeeb27fff5f84c8291b9a72229a9571154e
Henry-Bayly <84646983+Henry-Bayly@users.noreply.github.com>
2022-05-08 16:09:28 -0400

    update the runtime w stepcount

--- AKS.py
+++ AKS.py
@@ -5,6 +5,8 @@ import numpy as np
 def stepuno(n):
     for b in range(2,int(math.log(n,2))+1):
         for a in range(2, int(n**0.5)+1):
+            global stepcount
+            stepcount += 1
             print('a = '+str(a)+', b = '+str(b))
             if (n==a**b):
                 print('a^b = '+str(a)+'^'+str(b)+' equals '+str(n))
@@ -18,10 +20,12 @@ def stepdos(n):
     count = 0
     for r in range(2,max(3,int(math.log(n,2)**5)+1)):
         if count == int(math.log(n,2)**2)-1:
-            print("r = ",r-1, " satisfies the conditions for multplicative order.")
+            print(r-1, " Satisfies the conditions for multplicative order.")
             return r-1
         count = 0
         for k in range(1,int(math.log(n,2)**2)):
+            global stepcount
+            stepcount += 1
             count += 1
             print("Testing r = ", r, "with k = ", k)
             if (n**k) % r == 1 or (n**k) % r == 0:
@@ -31,6 +35,8 @@ def stepdos(n):
 def steptres(r,n):
     for a in range(2,r):
         print("Checking if GCD(a,n) is within the bounds, where a = ", a)
+        global stepcount
+        stepcount += 1
         if math.gcd(a,n) > 1 and math.gcd(a,n) < n :
             print("The GCD(a,n) is between 1 and n. Therefore ", n, " is composite.")
             return 'composite'
@@ -38,12 +44,17 @@ def steptres(r,n):
 
 def stepcuatro(n,r):
     if n <= r:
+        global stepcount
+        stepcount += 1
         print('n is less than or equal to r. Therefore n must be prime')
         return 'prime'
 
 
 def stepcinco(n,r):
     for a in range(1,int(totient(n)**0.5 * math.log(n,2))):
+        global stepcount
+        stepcount += 1
+        stepcount += 1
         left_hand_side = np.poly1d([1,a])**n
         right_hand_side = np.poly1d([1]+[0 for i in range(n-2)]+[a])
         divisor = np.poly1d([1]+[0 for i in range(r-2)]+[-1])
@@ -58,23 +69,49 @@ def stepcinco(n,r):
   
 
 n = 69 #composite
-
+stepcount = 0
+composite = False
 outputs = []
-outputs.append(stepuno(n))
-r = stepdos(n)
-outputs.append('lol')
-outputs.append(steptres(r,n))
-outputs.append(stepcuatro(n,r))
-outputs.append(stepcinco(n,r))
 count = 0
-for i in range(len(outputs)):
-    if outputs[i] == 'composite':
-        print('the number is composite, it failed step', i+1)
-        break
+one = stepuno(n)
+
+if(one == 'composite'):
+    composite = True
+else:
     count += 1
-if count == len(outputs):
+
+r = stepdos(n)
+if (composite == False):
+    three = steptres(r,n)
+    if(three == 'composite'):
+        composite = True
+    else:
+        count += 1
+if (composite == False):
+    four = stepcuatro(n,r)
+    if(four == 'composite'):
+        composite = True
+    else:
+        count += 1
+if (composite == False):
+    five = stepcinco(n,r)
+    if(fove == 'composite'):
+        composite = True
+    else:
+        count += 1
+
+
+if count == 4:
     print('the number is prime')
+else:
+    print('the number is composite. it failed step ', count+2)
+
+
     
+
+
+
+
 
 
 
